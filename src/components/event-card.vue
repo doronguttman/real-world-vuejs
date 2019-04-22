347d62c744808005671779d4a012bfd837045b00
Doron Guttman <dguttman@jacada.com>
2019-04-21 22:52:56 -0400

    + IE polyfills + tslint + extra tslint rules * renamed files

---
+++ src/components/event-card.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>{{ quote }}</h1>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        quote: "I want to be single"
+      };
+    }
+  };
+</script>
+
+<style>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <event-card></event-card>
   </div>
 </template>
+
+<script lang="ts">
+  import { Vue, Component } from "vue-property-decorator";
+  import EventCard from "@/components/event-card.vue";
+
+  @Component({
+    components: {
+      EventCard
+    }
+  })
+  export default class About extends Vue {}
+</script>
